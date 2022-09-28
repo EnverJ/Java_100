92baba3f33741044fe937a3b42cfc359e1f1f857
EnverJ <Enlem1987@gmail.com>
2022-09-27 22:30:03 -0600

    index

---
+++ src/java_100/StringEndWith_35.java
@@ -0,0 +1,11 @@
+package java_100;
+
+
+public class StringEndWith_35 {
+    public static void main(String[] args) {
+        String s1="I like Java";
+        System.out.println(s1.endsWith("r"));
+        System.out.println(s1.endsWith("Java"));
+        System.out.println(s1.endsWith("java"));
+    }
+}
