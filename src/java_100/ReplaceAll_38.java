634e48445cca40d6a97f756b62895a64b5d78c00
EnverJ <Enlem1987@gmail.com>
2022-09-28 21:43:06 -0600

    slpit

---
+++ src/java_100/ReplaceAll_38.java
@@ -0,0 +1,10 @@
+package java_100;
+
+public class ReplaceAll_38 {
+    public static void main(String[] args) {
+        String s1="Hi Java, Hi Javascript,Hi C#";
+        String replaceString=s1.replaceAll("Hi","Hello");
+        System.out.println(replaceString);
+
+    }
+}
