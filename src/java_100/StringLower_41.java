caf3f8e3cade642333625f92cfdaa389cd97c21b
EnverJ <Enlem1987@gmail.com>
2022-09-29 22:26:29 -0600

    trim

---
+++ src/java_100/StringLower_41.java
@@ -0,0 +1,8 @@
+package java_100;
+
+public class StringLower_41 {
+    public static void main(String[] args) {
+        String s1="Java IS MY FAVORITE LANGUAGE ";
+        System.out.println(s1.toLowerCase());
+    }
+}
---
+++ src/java_100/StringTrim_40.java
@@ -0,0 +1,9 @@
+package java_100;
+
+public class StringTrim_40 {
+    public static void main(String[] args) {
+        String s1=" Hello Java ";
+        System.out.println(s1+"!!!");
+        System.out.println(s1.trim()+"!!!");
+    }
+}
