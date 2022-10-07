8e8a2385e1bca3765062eaa5320d85f6205ae7c7
EnverJ <Enlem1987@gmail.com>
2022-10-06 22:10:30 -0600

    constructor

---
+++ src/java_100/constructor_47.java
@@ -0,0 +1,14 @@
+package java_100;
+
+public class constructor_47 {
+
+    constructor_47()
+    {
+        System.out.println("Constructor method called");
+    }
+
+    public static void main(String[] args) {
+        constructor_47 cp=new constructor_47();  // creating object
+
+    }
+}
