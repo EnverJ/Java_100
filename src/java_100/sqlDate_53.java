9ea3030418284d08025e0326f064b64102f7d294
EnverJ <Enlem1987@gmail.com>
2022-10-12 21:33:16 -0600

    String to sql data

--- src/java_100/sqlDate_53.java
+++ src/java_100/sqlDate_53.java
@@ -1,6 +1,6 @@
 package java_100;
 
-public class sqlData_53 {
+public class sqlDate_53 {
     public static void main(String[] args) {
         long millis=System.currentTimeMillis();
         java.sql.Date date=new java.sql.Date(millis);
