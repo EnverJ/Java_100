7aa7a68b739b9e9784246ee25a14711963c87145
EnverJ <Enlem1987@gmail.com>
2022-10-31 21:49:12 -0600

    convert data type

---
+++ src/java_100/StringToFloat_83.java
@@ -0,0 +1,9 @@
+package java_100;
+
+public class StringToFloat_83 {
+    public static void main(String[] args) {
+        String s="100.09";
+        float f=Float.valueOf(s);
+        System.out.println(f);   //100.09
+    }
+}
---
+++ src/java_100/StringToLong_82.java
@@ -0,0 +1,12 @@
+package java_100;
+
+public class StringToLong_82 {
+    public static void main(String[] args) {
+        String s="9999999999";
+        long l=Long.valueOf(s);
+        System.out.println(l);
+
+    }
+
+
+}
